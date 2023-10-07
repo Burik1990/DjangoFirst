8dca0922ebdc8e5b6b7ebe296fe94a38a6b51052
Burik1990 <bury1990@mail.ru>
2023-10-07 18:10:40 +0300

    dz_20_1

--- catalog/management/commands/filling.py
+++ catalog/management/commands/filling.py
@@ -1,25 +1,27 @@
-import  json
+import json
 
 from django.core.management import BaseCommand
 
 from catalog.models import Product, Category
-from config.settings import  BASE_DIR
+from config.settings import BASE_DIR
+
+
 class Command(BaseCommand):
-    help = '?????????? ???? ?????? ??????????'
 
     def handle(self, *args, **options):
         Product.objects.all().delete()
         Category.objects.all().delete()
 
-        with open(BASE_DIR/'data_category.json', encoding='utf-8') as fp:
+        with open(BASE_DIR / 'data_category.json', encoding='utf-8') as fp:
             category_data = json.load(fp)
             for item in category_data:
                 Category.objects.create(
-                    pk= item['pk'],
-                    name = item["fields"]["name"],
-                    description = item["fields"]["description"]
+                    pk=item['pk'],
+                    name=item["fields"]["name"],
+                    description=item["fields"]["description"]
                 )
-        with open(BASE_DIR/'data.json', encoding='utf-8') as fp:
+
+        with open(BASE_DIR / 'data.json', encoding='utf-8') as fp:
             product_data = json.load(fp)
             for item in product_data:
                 category_pk = item["fields"]["category"]
