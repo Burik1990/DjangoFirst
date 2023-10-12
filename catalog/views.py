5d5c29d91bf3e63a5770714906c1aff24e45dc80
Burik1990 <bury1990@mail.ru>
2023-10-12 17:21:48 +0300

    dz_20_2

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Category
+
 
 def contacts(request):
-    print(request.POST)
-    return render(request, 'catalog/contacts.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} ({phone}) : {message}')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    category_list = Category.objects.all()
+    context = {
+        'object_list': category_list,
+        'title': 'Категория товара'
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def product(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Продукты'
+    }
+    return render(request, 'catalog/product.html', context)
